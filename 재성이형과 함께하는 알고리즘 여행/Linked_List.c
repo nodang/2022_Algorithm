7422e9d351f131fbe96c8d5847df746c4edcca57
nodang <noj7413@naver.com>
2022-01-22 02:11:28 +0900

    220122_linked_list

--- 재성이형과 함께하는 알고리즘 여행/Linked_List.c
+++ 재성이형과 함께하는 알고리즘 여행/Linked_List.c
@@ -7,29 +7,31 @@ typedef struct Node
 	int data;
 }st_node;
 
-st_node* init_node(st_node* front_node)
+int print_err_address(char function[], char name[])
 {
-	if (front_node == NULL)
-	{
-		printf("Err[front node] : Front node's address is NULL");
-		return NULL;
-	}
+	printf("Err[%s] : %s's address is NULL\n", function, name);
+	return -1;
+}
+
+int print_err_memory(char name[])
+{
+	printf("Err[%s] : Memory isn't enough\n", name);
+	return NULL;
+}
+
+st_node* init_node()
+{
 	st_node* node = malloc(sizeof(st_node));
-	if (node == NULL)
-	{
-		printf("Err[malloc] : Memory isn't enough");
-		return NULL;
-	}
+	if (node == NULL)		
+		return print_err_memory("init_node", "node_malloc");
+
 	return node;
 }
 
 int change_data(st_node* node, int data)
 {
-	if (node == NULL)
-	{
-		printf("Err[node] : Node's address is NULL");
-		return -1;
-	}
+	if (node == NULL)		
+		return print_err_address("change_node", "node");
 
 	node->data = data;
 
@@ -38,10 +40,13 @@ int change_data(st_node* node, int data)
 
 int add_node(st_node* front_node, int data)
 {
-	st_node* node = init_node(front_node);
-	if (node == NULL)		return -1;
+	if (front_node == NULL)				
+		return print_err_address("add_node", "front node");
 
-	if (change_data(node, data) == -1)	return -1;
+	st_node* node = init_node();
+	if (node == NULL)	return -1;
+
+	if (-change_data(node, data))	return -1;
 	node->p_next = NULL;
 	front_node->p_next = node;
 
@@ -50,10 +55,11 @@ int add_node(st_node* front_node, int data)
 
 int exit_node(st_node* head_node)
 {
-	if (head_node == NULL)		return -1;
+	if (head_node == NULL)				
+		return print_err_address("exit_node", "head node");
 
 	st_node* curr = head_node;
-	while (curr->p_next->p_next != NULL)		curr = curr->p_next;
+	while (curr->p_next->p_next != NULL)	curr = curr->p_next;
 	
 	st_node* rm_node = curr->p_next;
 
@@ -67,11 +73,8 @@ int exit_node(st_node* head_node)
 
 int print_node(st_node* head_node)
 {
-	if (head_node == NULL)
-	{
-		printf("Err[head node] : Head node's address is NULL");
-		return -1;
-	}
+	if (head_node == NULL)		
+		return print_err_address("printf_node", "head node");
 
 	st_node* curr = head_node;
 	while (curr != NULL)
@@ -93,15 +96,15 @@ int main(void)
 	st_node *p_curr = p_head;
 	for (int i = 1; i <= 10; i++)
 	{
-		if (add_node(p_curr, i) == -1)		return 0;
+		if (-add_node(p_curr, i))		return 0;
 		p_curr = p_curr->p_next;
 	}
 
-	if (print_node(p_head) == -1)			return 0;
+	if (-print_node(p_head))			return 0;
 	for (int i = 1; i <= 10; i++)
-		if (exit_node(p_head) == -1)		return 0;
+		if (-exit_node(p_head))			return 0;
 
-	if (print_node(p_head) == -1)			return 0;
+	if (-print_node(p_head))			return 0;
 
 	free(p_head);
 
