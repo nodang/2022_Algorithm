c977a40087dc501f6ef184dbea8a093ece19f4c3
nodang <noj7413@naver.com>
2022-01-23 22:49:04 +0900

    220123_linked_list

--- 재성이형과 함께하는 알고리즘 여행/Linked_List.c
+++ 재성이형과 함께하는 알고리즘 여행/Linked_List.c
@@ -1,3 +1,4 @@
+#include <memory.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,48 +8,69 @@ typedef struct Node
 	int data;
 }st_node;
 
-int print_err_address(char function[], char name[])
+static int _print_err_address(const char* function, const char* name)
 {
 	printf("Err[%s] : %s's address is NULL\n", function, name);
 	return -1;
 }
-
-int print_err_memory(char name[])
+// read only : const
+static inline void _print_err_memory(const char* name)
 {
 	printf("Err[%s] : Memory isn't enough\n", name);
-	return NULL;
 }
 
-st_node* init_node()
+st_node* init_node(void)	//c : (void) | c++ : ()
 {
-	st_node* node = malloc(sizeof(st_node));
+	st_node* node = calloc(1, sizeof(st_node));	// (st_node*)malloc
 	if (node == NULL)		
-		return print_err_memory("init_node", "node_malloc");
+	{
+		_print_err_memory("init_node");
+		return NULL;
+	}
 
-	node->p_next = NULL;
+	//memset(node, 0, sizeof(st_node));			// 확장성을 고려하여 코드를 설계할 것
+	//malloc + memset = calloc		속도 차이가 있음
 
 	return node;
 }
 
-int change_data(st_node* node, int data)
+int exit_node(st_node** rm_node)
+{
+	if(rm_node == NULL || *rm_node == NULL)
+		return -1;
+
+	free(*rm_node);
+	*rm_node = NULL;
+
+	return 0;
+}
+
+int change_data(st_node* node,const int data)
 {
 	if (node == NULL)		
-		return print_err_address("change_node", "node");
+		return _print_err_address("change_node", "node");
 
 	node->data = data;
 
 	return 0;
 }
 
-int add_node(st_node* front_node, int data)
+//inline으로 선언할 시 헤더에 함수를 통째로 선언할 것
+
+int add_node(st_node* front_node, const int data)
 {
 	if (front_node == NULL)				
-		return print_err_address("add_node", "front node");
+		return _print_err_address("add_node", "front node");
 
 	st_node* node = init_node();
-	if (node == NULL)	return -1;
+	if (node == NULL)
+		return -1;
 
-	if (-change_data(node, data))	return -1;
+	if (change_data(node, data) == -1)	
+		return -1;
+
+// 함수를 직접 호출하는 것은 지양할 것 | 한정된 메모리 시스템가 아니면 지양할 것
+// 코드의 규칙성을 지킬 것
 
 	if (front_node->p_next == NULL)
 	{
@@ -64,19 +86,17 @@ int add_node(st_node* front_node, int data)
 	return 0;
 }
 
-int exit_node(st_node* front_node)
+int delete_node(st_node** front_node)
 {
-	if (front_node == NULL)
-		return print_err_address("exit_node", "front_node");
-	
-	st_node* rm_node = front_node->p_next;
-	if (rm_node == NULL)
-		return print_err_address("exit_node", "rm_node");
+	if (*front_node == NULL || (*front_node)->p_next == NULL)
+		return _print_err_address("exit_node", "front_node");
 
-	front_node->p_next = rm_node->p_next;
+	st_node* rm_node = (*front_node)->p_next;
+
+	(*front_node)->p_next = rm_node->p_next;
 	rm_node->p_next = NULL;
 
-	free(rm_node);
+	exit_node(&rm_node);
 
 	return 0;
 }
@@ -84,7 +104,7 @@ int exit_node(st_node* front_node)
 static int print_node(st_node* head_node)
 {
 	if (head_node == NULL)		
-		return print_err_address("printf_node", "head node");
+		return _print_err_address("printf_node", "head node");
 
 	st_node* curr = head_node;
 	while (curr != NULL)
@@ -99,26 +119,27 @@ static int print_node(st_node* head_node)
 
 int main(void)
 {
-	st_node* p_head = NULL;
-	p_head = init_node();
+	st_node* p_head = init_node();
+	if(p_head == NULL)
+		return 0;
 
 	st_node *p_curr = p_head;
 	for (int i = 1; i <= 10; i++)
 	{
-		if (-add_node(p_curr, i))		return 0;
+		if (add_node(p_curr, i) == -1)		return 0;
 		p_curr = p_curr->p_next;
 	}
 
-	if (-print_node(p_head))			return 0;
+	if (print_node(p_head) == -1)			return 0;
 
 	for (int i = 1; i <= 10; i++)
 	{
-		if (-exit_node(p_head))			return 0;
+		if (delete_node(&p_head) == -1)		return 0;
 	}
 
-	if (-print_node(p_head))			return 0;
+	if (print_node(p_head) == -1)			return 0;
 
-	free(p_head);
+	exit_node(&p_head);		// 함수로 처리 또는 malloc으로 직접 선언해서 헷갈리는 현상을 방지할 것
 
 	return 0;
 }
